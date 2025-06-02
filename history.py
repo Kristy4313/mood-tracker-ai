e1a313ef2562245cb33db72168e027c8786e0f35
Kristy1343 <kristelle.pabon@upr.edu>
2025-06-02 19:28:51 -0400

    Initiated the view history function... still working on it

---
+++ history.py
@@ -0,0 +1,12 @@
+"""
+Function meant to display mood history... coming soon
+"""
+
+import mood
+
+
+def view_history():
+    print("\n--- Mood History ---")
+    print("Feature coming soon! No mood history available yet.\n")
+
+
