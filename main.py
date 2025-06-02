740698b801651eba2913bb925189d3171a690746
Kristy1343 <kristelle.pabon@upr.edu>
2025-06-02 19:26:32 -0400

    Added the menu

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-def get_mood():
-    mood = input("How are you feeling today? ")
-    print(f"Thanks for sharing your mood: {mood}")
+from mood import get_mood
+from history import view_history
 
+def main_menu():
+    while True:
+        print("\nWelcome to Mood Tracker AI") # menu first line
+
+        print("\nPlease select an option:\n")
+        print("1 - Enter today's mood\n")
+        print("2 - View mood history\n")
+        print("3 - Exit\n")
+        
+        choice = input("Enter your choice (1-3): ")
+        
+        if choice == "1":
+            get_mood()
+        elif choice == "2":
+            view_history()
+        elif choice == "3":
+            print("Goodbye!")
+            break
+        else:
+            print("Sorry, that's an invalid choice. Please try again.")
+
+        
 if __name__ == "__main__":
-    get_mood()
+    main_menu()
+        
+
+
