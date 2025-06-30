dca5bff6623addb2a0301174006c89e75e402167
Kristy1343 <kristelle.pabon@upr.edu>
2025-06-30 08:32:37 -0400

    Completed the view history function

--- src/history.py
+++ src/history.py
@@ -4,12 +4,21 @@ import src.mood as mood
 Function meant to display mood history... coming soon
 """
 
-
+"""
+1.Open data/mood_log.txt (using files)
+2. Read and print each mood entry (using files)
+3. else -> message "No page available, try logging in a mood first" (conditionals)
+"""
 
 def view_history():
-    print("\n--- Mood History ---")
-    print("\nFeature coming soon! No mood history available yet.")
-    print("\n --------------------------------------------------")
+    with open("data/mood_log.txt", "r") as file:
+        mood_entries = file.readlines() # read all lines in mood file
+        if mood_entries:
+            print("\n--- Mood History ---")
+            for entry in mood_entries:
+                print(entry.strip()) #display the mood log
+        else:
+            print("\nNo mood history available. Please log your mood first.")
 
 
 if __name__ == "__main__":
